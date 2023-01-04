3bfee97a551e1a3ba472a5963a2b2d152814dc2b
Nguyễn Thái Hưnng <hung.nt20@gmail.com>
2023-01-04 22:43:58 +0700

    hello

--- src/main/java/com/example/elasticsearch1/Elasticsearch1Application.java
+++ src/main/java/com/example/elasticsearch1/Elasticsearch1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Elasticsearch1Application {
     public static void main(String[] args) {
+        System.out.printf("Hello world");
         SpringApplication.run(Elasticsearch1Application.class, args);
     }
 }
--- src/main/java/com/example/elasticsearch1/controller/MainController.java
+++ src/main/java/com/example/elasticsearch1/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.example.elasticsearch1.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("log")
 public class MainController {
 
-    @GetMapping("")
-    public void request(){
-        log.info("123123");
+    @PostMapping("")
+    public ResponseEntity<?> request(){
+        return ResponseEntity.ok("Hello this is log");
     }
 }
